d893bbec17766760c1bc214888dda4bdb0bfe23d
s2c <varun@alchemy.email>
2020-01-30 10:44:02 -0500

    Minor changes

--- valueIteration.py
+++ valueIteration.py
@@ -7,16 +7,19 @@ class valueIteration(object):
        t: near 0 constant so to avoid 0+,0- errors
 
 	"""
-	def __init__(self, stateRange=[None,None],numInterval=8, C = 1000):
+	def __init__(self, stateRange=[None,None],numInterval=8, C = 1000, game = G, gamma = 0.5):
 		super(valueIteration, self).__init__()
 		self.statesActual = np.linspace(stateRange[0],stateRange[1],numInterval+1)		
 		self.numInterval = numInterval
 		self.gap = self.statesActual[1]-self.statesActual[0]
 		self.V = dict.fromkeys(v.statesActual,0) # initialize everything to 0 in the beginning
 		self.C = C
+		self.G = g
+		self.gamma = gamma
 
 	def nearestState(self, curState):
 		# Cleaner nearest neighbour cause single dimensional
+		# Should be O(1)
 		if (curState % self.gap) > 0.5 * self.gap:
 			nearest = curState - (curState % diff) + diff
 		else:
@@ -28,8 +31,13 @@ class valueIteration(object):
 			print ("Something messed up")
 			return -4
 
-	def Bellman(self,curState):
-		pass
+	def childMaxCalc(self,curState):
+		rewards = [0] * self.G.actions 
+		for i,action in enumerate(self.G.actions):
+			rewards[i] = self.G.calcReward(curState,action)
+		return (rewards)
+
+
 
 
 
