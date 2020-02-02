588266b72ad71daf56279d84aec6e95dbb5e279e
Varun Somani <varunsomani95@gmail.com>
2020-02-02 13:12:15 -0800

    Model Changes

--- EIS.py
+++ EIS.py
@@ -2,7 +2,7 @@ from valueIteration import valueIteration
 from EISGame import EISGame
 from discretizer import discretizer
 from sparseSampling import sparseSampling
-
+from bisect import bisect_left
 
 class SLModel(object):
     """ SLModel"""
@@ -17,5 +17,24 @@ class SLModel(object):
         return self.V[stateActual]
 
     def updateValues(self, data):
-        for state, value in data:
-            self.V[self.d.nearest(state)] = value
+        data = dict(data)  # Covert to dictionary for the O(1) lookup
+        for state in self.V.keys():
+            # find closest state in data
+            closest = self.__findClosest(state, data)
+            self.V[state] = data[closest]
+
+    def __findClosest(self, point, dataStates):
+        sortedData = sorted(dataStates.items())
+        sortedData, _ = zip(*sortedData)
+        pos = bisect_left(sortedData, point)
+        if pos == 0:
+            return sortedData[0]
+        if pos == len(sortedData):
+            return sortedData[-1]
+        before = sortedData[pos - 1]
+        after = sortedData[pos]
+        if after - point < point - before:
+            final = after
+        else:
+            final = before
+        return final
